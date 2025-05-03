e0bf976c2e570f8346ba6f4c730b20db125823b1
Hafiz Sheikh <hafizs0786@gmail.com>
2025-05-03 10:52:53 +0530

    Assignment 1 Solution

---
+++ Assign1.java
@@ -0,0 +1,22 @@
+public class Assign1 {
+    public static void q1() {
+        System.out.println("Hello Students");
+    }
+    public static void q2() {
+        System.out.println("Hello\nStudents");
+    }
+    public static void q3() {
+        System.out.println("\"MySirG\"");
+    }
+    public static void q4() {
+        System.out.println("\"Teacher's Day\"");
+    }
+    public static void q5() {
+        int x=5;        
+        System.out.println("Value of x is: "+x);
+    }
+    public static void q6() {
+        float y=6.5f;        
+        System.out.println("Value of y is: "+y);
+    }
+}
--- Driver.java
+++ Driver.java
@@ -1,5 +1,10 @@
 public class Driver {
     public static void main(String[] args) {
-        
+        Assign1.q1();
+        Assign1.q2();
+        Assign1.q3();
+        Assign1.q4();
+        Assign1.q5();
+        Assign1.q6();
     }
 }
